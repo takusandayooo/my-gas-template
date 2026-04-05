2878fe4dc9ea0f5ab21cd8051765ba30f391311a
たくや <takuyadayo014@gmail.com>
2026-04-05 17:26:47 +0900

    chore: swcを用いてビルドするように変更

--- build.js
+++ build.js
@@ -1,10 +1,46 @@
-import { build } from "esbuild";
-import { GasPlugin } from "esbuild-gas-plugin";
+import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { rollup } from "rollup";
+import { swc } from "rollup-plugin-swc3";
 
-await build({
-  entryPoints: ["src/index.ts"],
-  bundle: true,
-  target: "es2019",
-  outfile: "build/index.js",
-  plugins: [GasPlugin],
-}).catch(() => process.exit(1));
+/** GAS向けに export 構文を除去するプラグイン */
+function gasPlugin() {
+	return {
+		name: "gas",
+		renderChunk(code) {
+			return {
+				code: code
+					.replace(
+						/^export\s+(?=(?:async\s+)?function|class|const|let|var)/gm,
+						"",
+					)
+					.replace(/^export\s*\{[^}]*\}\s*;?\s*$/gm, "")
+					.replace(/^export\s+default\s+/gm, ""),
+				map: null,
+			};
+		},
+	};
+}
+
+const bundle = await rollup({
+	input: "src/index.ts",
+	plugins: [
+		nodeResolve(),
+		swc({
+			jsc: {
+				target: "es2019",
+				parser: {
+					syntax: "typescript",
+				},
+			},
+			sourceMaps: false,
+		}),
+		gasPlugin(),
+	],
+});
+
+await bundle
+	.write({
+		file: "build/index.js",
+		format: "es",
+	})
+	.catch(() => process.exit(1));
--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,6 @@ import { main } from "./app";
  * @file GASエディタから実行できる関数を定義する
  */
 
+
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
-declare const global: any;
-global.main = main;
+(globalThis as any).main = main;
